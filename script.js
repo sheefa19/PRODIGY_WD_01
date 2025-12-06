664148574bc22880a905a9477165f5266b894c4d
sheefa19 <shaikhshifa15214@gmail.com>
2025-12-06 15:59:31 +0530

    updated tasks

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-const navbar = document.getElementById("navbar");
-
-window.addEventListener("scroll", function () {
-    if (window.scrollY > 30) {
-        navbar.classList.add("scrolled");
+// Navbar scroll animation
+window.addEventListener('scroll', () => {
+    const navbar = document.getElementById('navbar');
+    if(window.scrollY > 60){
+        navbar.classList.add('scrolled');
     } else {
-        navbar.classList.remove("scrolled");
+        navbar.classList.remove('scrolled');
     }
+});
+
+// Contact form alert
+document.getElementById("contactForm").addEventListener("submit", (e)=>{
+  e.preventDefault();
+  alert("Thank you! Your message has been sent successfully 😊");
+  e.target.reset();
 });
